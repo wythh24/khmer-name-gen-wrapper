5b7b6fe183fff1f77d7f5040e107170a53d2bdad
Davit <reelmad2@gmail.com>
2024-03-21 11:11:04 +0700

    refactor: add document and example usage

--- src/Extensions/GenerateNameWrapperExtension.cs
+++ src/Extensions/GenerateNameWrapperExtension.cs
@@ -7,6 +7,11 @@ public static class GenerateNameWrapperExtension
 {
     private static GeneratorBase _GeneratorBase { get; set; }
 
+    /// <summary>
+    /// Generates a non-Khmer name using the provided GenerateNameWrapper.
+    /// </summary>
+    /// <param name="generator">An instance of GenerateNameWrapper</param>
+    /// <returns>A string representing the generated non-Khmer name</returns>
     public static string RealNameGenerator(this GenerateNameWrapper generator)
     {
         _GeneratorBase = new RealNameGenerator();
